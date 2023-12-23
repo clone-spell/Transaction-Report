b7426a9e57b7579fecca66e904828b091fc6d34a
91834 <91834@BABLU>
2023-12-24 01:42:04 +0530

    Add project files.

---
+++ Transactions/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Transactions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSiteName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.cbMonths = new System.Windows.Forms.ComboBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtOut = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSiteName
+            // 
+            this.lblSiteName.AutoSize = true;
+            this.lblSiteName.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSiteName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSiteName.Location = new System.Drawing.Point(13, 26);
+            this.lblSiteName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSiteName.Name = "lblSiteName";
+            this.lblSiteName.Size = new System.Drawing.Size(0, 15);
+            this.lblSiteName.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(386, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Month:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.cbYear);
+            this.panel1.Controls.Add(this.cbMonths);
+            this.panel1.Controls.Add(this.btnGenerate);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblSiteName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 66);
+            this.panel1.TabIndex = 1;
+            // 
+            // cbYear
+            // 
+            this.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(258, 19);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(121, 28);
+            this.cbYear.TabIndex = 0;
+            // 
+            // cbMonths
+            // 
+            this.cbMonths.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMonths.FormattingEnabled = true;
+            this.cbMonths.Location = new System.Drawing.Point(448, 19);
+            this.cbMonths.Name = "cbMonths";
+            this.cbMonths.Size = new System.Drawing.Size(121, 28);
+            this.cbMonths.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(651, 14);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(121, 38);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(211, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Year:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnClear);
+            this.panel2.Controls.Add(this.btnCopy);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 503);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 58);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSave.Location = new System.Drawing.Point(336, 9);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(121, 38);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnClear.Location = new System.Drawing.Point(208, 9);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 38);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCopy.Location = new System.Drawing.Point(463, 9);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(121, 38);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar.Location = new System.Drawing.Point(0, 493);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(784, 10);
+            this.progressBar.TabIndex = 3;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOut.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(0, 69);
+            this.txtOut.Margin = new System.Windows.Forms.Padding(0);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(784, 424);
+            this.txtOut.TabIndex = 5;
+            this.txtOut.Text = "";
+            this.txtOut.WordWrap = false;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transaction Report";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSiteName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbMonths;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtOut;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Transactions/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Transactions
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = $"Data Source={Environment.MachineName};Initial Catalog=WelkinATP;Integrated Security=SSPI;";
+        private string[] monthNames = new string[]
+            {
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"
+            };
+        private string month = "";
+        private string year = "";
+        private string strOutput = "";
+        private string siteName = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            setDate();
+            backgroundWorker.WorkerReportsProgress = true;
+
+            btnCopy.Enabled = false;
+            btnSave.Enabled = false;
+        }
+
+        //button clicks
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            month = (Array.IndexOf(monthNames, cbMonths.Text) + 1).ToString("d2");
+            year = cbYear.Text;
+
+            if (!backgroundWorker.IsBusy)
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            strOutput = string.Empty;
+            txtOut.Text = string.Empty;
+            progressBar.Value = 0;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FileName = cbMonths.Text+" ("+lblSiteName.Text+").txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    SaveToTxtFile(filePath, txtOut.Text);
+                }
+            }
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if(txtOut.Text != "")
+            {
+                try
+                {
+                    Clipboard.SetText(txtOut.Text);
+                    btnCopy.Enabled = false;
+                    btnCopy.Text = "Copied";
+                }
+                catch { }
+            }
+        }
+
+        //functions
+        //geting on & off times
+        public static string GetFirstAndLastLineChars(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return "not found\tnot found\tnull";
+
+            string firstChars = "";
+            string lastChars = "";
+
+            try
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Get the first line and its first 8 characters
+                if (lines.Length > 0)
+                {
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string firstLine = lines[i];
+                        firstChars = firstLine.Length >= 8 ? firstLine.Substring(0, 8) : firstLine;
+                        if (firstChars.Contains(":") && firstChars.Length == 8 && !firstChars.Contains(" ") && !firstChars.Contains("\t"))
+                            break;
+                    }
+                }
+
+                // Get the last line and its first 8 characters
+                if (lines.Length > 1)
+                {
+                    for (int i = lines.Length - 1; i >= 0; i--)
+                    {
+                        string lastLine = lines[i];
+                        lastChars = lastLine.Length >= 8 ? lastLine.Substring(0, 8) : lastLine;
+                        if (lastChars.Contains(":") && lastChars.Length == 8 && !lastChars.Contains(" ") && !lastChars.Contains("\t"))
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle file reading errors, if any
+                Console.WriteLine($"Error reading the file: {ex.Message}");
+            }
+
+
+            if (!(firstChars.Contains(":") && firstChars.Length == 8 && !firstChars.Contains(" ") && !firstChars.Contains("\t")) || !(lastChars.Contains(":") && lastChars.Length == 8 && !lastChars.Contains(" ") && !lastChars.Contains("\t")))
+            {
+                return "not found\tnot found\tnull";
+            }
+
+
+            try
+            {
+                TimeSpan duration = DateTime.Parse(lastChars) - DateTime.Parse(firstChars);
+                return firstChars +"\t"+ lastChars + "\t" + duration.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // Concatinate the first 8 characters of the first and last lines
+            return firstChars +"\t"+ lastChars + "\t" +"null";
+        }
+
+        //ui year and month
+        private void setDate()
+        {
+            DateTime currentDate = DateTime.Now;
+            DateTime prevMonth = currentDate.AddMonths(-1);
+            int month = prevMonth.Month;
+            int year = prevMonth.Year;
+            cbYear.Items.Clear();
+
+            cbYear.Items.Add(year-3);
+            cbYear.Items.Add(year-2);
+            cbYear.Items.Add(year-1);
+            cbYear.Items.Add(year);
+            cbYear.Items.Add(year+1);
+            cbMonths.DataSource = monthNames;
+
+            cbYear.SelectedItem = year;
+            cbMonths.SelectedItem = monthNames[month - 1];
+        }
+
+        //save to txt method
+        private void SaveToTxtFile(string filePath, string text)
+        {
+            try
+            {
+                // Write the text to the file
+                File.WriteAllText(filePath, text);
+                MessageBox.Show("Text saved to file successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while saving the file:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //background worker
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int noOfDays = DateTime.DaysInMonth(Convert.ToInt32(year), Convert.ToInt32(month));
+            string totalMonthlyTran = "";
+
+            for (int i = 1; i <= noOfDays; i++)
+            {
+                string date = year+"-"+month+"-"+i.ToString("d2");
+
+                string day = i.ToString("d2");
+                string logPath = $"C:\\V_Services\\data\\{year}{month}{day}\\Logfile.log"; ;
+                string onOffTime = GetFirstAndLastLineChars(logPath);
+
+                string cspName1 = "null";
+                string cspName2 = "null";
+                string cspTran1 = "null";
+                string cspTran2 = "null";
+                string totalDayWiseTran = "null";
+
+
+
+                try
+                {
+                    //geting site name & monthly trans
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = "Select Office_Name from fSettings";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    siteName = reader.GetString(0);
+                                }
+                            }
+                        }
+                        query = $"Select * from transactions where pay_dt >='{year}-{month}-01 00:00:00.000' and pay_dt <='{year}-{month}-{noOfDays} 23:59:59.999'";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                int rows = 0;
+                                while (reader.Read())
+                                {
+                                    rows++;
+                                }
+                                totalMonthlyTran = rows.ToString();
+                                if(rows < 1)
+                                {
+                                    MessageBox.Show("No Transaction Found!", "Empty", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    break;
+                                }
+                            }
+                        }
+                        conn.Close();
+                    }
+
+
+
+                    //geting csp name
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = $"select DISTINCT CSPName from Transactions where pay_dt >='{date} 00:00:00.000' and pay_dt <='{date} 23:59:59.999'";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            SqlDataReader reader = cmd.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                if (cspName1 != "null")
+                                {
+                                    cspName2 = reader.GetString(0);
+                                    break;
+                                }
+                                else
+                                    cspName1 = reader.GetString(0);
+                            }
+                        }
+                        conn.Close();
+                    }
+
+
+
+                    //geting transactions by date & csp name
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        //csp 1
+                        string query = $"Select * from transactions where pay_dt >='{date} 00:00:00.000' and pay_dt <='{date} 23:59:59.999' and CSPName='{cspName1}'";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                int rows = 0;
+                                while (reader.Read())
+                                {
+                                    rows++;
+                                }
+                                cspTran1 = rows.ToString();
+                            }
+                        }
+                        //csp 2
+                        query = $"Select * from transactions where pay_dt >='{date} 00:00:00.000' and pay_dt <='{date} 23:59:59.999' and CSPName='{cspName2}'";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                int rows = 0;
+                                while (reader.Read())
+                                {
+                                    rows++;
+                                }
+                                cspTran2 = rows.ToString();
+                            }
+                        }
+                        //daywise
+                        query = $"Select * from transactions where pay_dt >='{date} 00:00:00.000' and pay_dt <='{date} 23:59:59.999'";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                int rows = 0;
+                                while (reader.Read())
+                                {
+                                    rows++;
+                                }
+                                totalDayWiseTran = rows.ToString();
+                            }
+                        }
+                        conn.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show($"Details: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                strOutput += siteName + "\t" + date + "\t" + onOffTime + "\t" + cspName1 + "\t" + cspTran1 + "\t" + cspName2 + "\t" + cspTran2 + "\t" + totalDayWiseTran+ "\t" + totalMonthlyTran + Environment.NewLine;
+
+                System.Threading.Thread.Sleep(1);
+
+                // Report progress                
+                int progressPercentage = (int)((double)i / noOfDays * 100);
+                backgroundWorker.ReportProgress(progressPercentage);
+            }
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            txtOut.Text = strOutput;
+            lblSiteName.Text = siteName;
+
+            btnCopy.Enabled = true;
+            btnCopy.Text = "Copy";
+            btnSave.Enabled = true;
+        }
+    }
+}
